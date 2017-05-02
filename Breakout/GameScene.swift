669c27b780ae23d5f1378a878840738e6f899b55
student3 <student3@jh-mba158-12.d214.org>
2017-05-02 09:45:11 -0500

    Added borders and fixed velocities

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -17,64 +17,63 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
         
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
         bottomPaddle = self.childNode(withName: "bottomPaddle") as!
         SKSpriteNode
-        ball = self.childNode(withName: "ball") as! SKSpriteNode
         
-        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        ball.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 50))
+        
+        let border = SKPhysicsBody(edgeLoopFrom: (view.scene?.frame)!)
         border.friction = 0
         self.physicsBody = border
         
         
-        let bottomLeft = CGPoint(x: frame.origin.x, y: frame.origin.y)
-        let bottomRight = CGPoint(x: -frame.origin.x, y: frame.origin.y)
-        let topLeft = CGPoint(x: frame.origin.x, y: -frame.origin.y)
-        let topRight = CGPoint(x: -frame.origin.x, y: -frame.origin.y)
-        
-        let bottom = SKNode()
-        bottom.name = "bottom"
-        bottom.physicsBody = SKPhysicsBody(edgeFrom: bottomLeft, to: bottomRight)
-        addChild(bottom)
-        
-        
-        let top = SKNode()
-        top.name = "top"
-        bottom.physicsBody = SKPhysicsBody(edgeFrom: topLeft, to: topRight)
-        addChild(top)
-        
-        
-        let left = SKNode()
-        left.name = "left"
-        bottom.physicsBody = SKPhysicsBody(edgeFrom: bottomLeft, to: topLeft)
-        addChild(left)
-        
-        let right = SKNode()
-        right.name = "right"
-        bottom.physicsBody = SKPhysicsBody(edgeFrom: bottomRight, to: topRight)
-        addChild(right)
+//        let bottomLeft = CGPoint(x: frame.origin.x, y: frame.origin.y)
+//        let bottomRight = CGPoint(x: -frame.origin.x, y: frame.origin.y)
+//        let topLeft = CGPoint(x: frame.origin.x, y: -frame.origin.y)
+//        let topRight = CGPoint(x: -frame.origin.x, y: -frame.origin.y)
+//        
+//        let bottom = SKNode()
+//        bottom.name = "bottom"
+//        bottom.physicsBody = SKPhysicsBody(edgeFrom: bottomLeft, to: bottomRight)
+//        addChild(bottom)
+//        
+//        
+//        let top = SKNode()
+//        top.name = "top"
+//        bottom.physicsBody = SKPhysicsBody(edgeFrom: topLeft, to: topRight)
+//        addChild(top)
+//        
+//        
+//        let left = SKNode()
+//        left.name = "left"
+//        bottom.physicsBody = SKPhysicsBody(edgeFrom: bottomLeft, to: topLeft)
+//        addChild(left)
+//        
+//        let right = SKNode()
+//        right.name = "right"
+//        bottom.physicsBody = SKPhysicsBody(edgeFrom: bottomRight, to: topRight)
+//        addChild(right)
         
         
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        
-        let touch = touches.first!
-        let location = touch.location(in: self)
-        bottomPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
+            bottomPaddle.position.x = touchLocation.x
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-
-        let touch = touches.first!
-        let location = touch.location(in: self)
-        
-        bottomPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
-        
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
+            bottomPaddle.position.x = touchLocation.x
+        }
     }
 
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
     }
 }
